29d323626d67838260474fb20a780067484e0585
aUsrName <398823436@qq.com>
2025-02-15 18:03:59 +0800

    02

--- pythonProject/testcases/test_api.py
+++ pythonProject/testcases/test_api.py
@@ -1,7 +1,10 @@
 import time
 
+import allure
 
 
+@allure.epic('项目名称：通用接口框架参考')
+@allure.feature('模块名称：用户管理')
 class TestApi:
 
     # def setup_class(self):
@@ -15,12 +18,32 @@ class TestApi:
     #
     # def teardown_method(self):
     #     print('每个用例之后操作，每个用例执行一次')
-
-    def test_login(self):
-        print('登录测试用例')
+    @allure.story('接口名称：登录接口')
+    @allure.title('用例名称：验证登录接口成功返回数据')
+    @allure.severity(allure.severity_level.BLOCKER)
+    @allure.description('用户的登录接口成功用例')
+    @allure.link('接口地址')
+    @allure.issue('bug地址')
+    @allure.testcase('用例地址')
+    def test_login1(self):
+        allure.step('第一步')
+        print('登录测试成功用例')
+        allure.step('第二步')
         # raise Exception('登录用例失败')
 
+    @allure.story('接口名称：登录接口')
+    @allure.title('用例名称：验证登录接口失败')
+    def test_login2(self):
+        print('登录测试失败用例')
+        # raise Exception('登录用例失败')
+
+    @allure.story('接口名称：注册接口')
     def test_register(self,exe_order):
+        allure.dynamic.title('用例名称：注册测试用例')
+        allure.dynamic.description('注册接口测试用例')
+        allure.dynamic.link('接口地址')
+        allure.dynamic.issue('bug地址')
+        allure.dynamic.testcase('用例地址')
         print('注册测试用例')
 
 
