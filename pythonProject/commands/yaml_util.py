5f0ebe6d0cecf41283259c64fc9060515d08a37b
aUsrName <398823436@qq.com>
2025-02-27 04:48:45 +0800

    03

--- pythonProject/commands/yaml_util.py
+++ pythonProject/commands/yaml_util.py
@@ -7,6 +7,19 @@ def read_yaml(yaml_path):
         value = yaml.safe_load(f)
         return value
 
+#用于写入yaml
+def write_yaml(yaml_path):
+    with open(yaml_path, encoding='utf-8', mode='w') as f:
+        data = {'teacher':[{'name1': 'test1'},{'name2': 'test2'}]}
+        yaml.safe_dump(data, f, allow_unicode=True)
+
+#用于清空yaml
+def clean_yaml(yaml_path):
+    #一定要用 w ，表示替换
+    with open(yaml_path, encoding='utf-8', mode='w') as f:
+        pass
 
 if __name__ == '__main__':
     print(read_yaml('../testcases/test_api.yaml'))
+    # write_yaml('../testcases/test_api.yaml')
+    # clean_yaml('../testcases/test_api.yaml')
--- pythonProject/testcases/test_beta.py
+++ pythonProject/testcases/test_beta.py
@@ -1,7 +1,16 @@
 import time
 import pytest
 
-class Test0202:
+from commands.yaml_util import read_yaml
 
-    def test_0202(self):
-        print('测试文件2')
+
+class Test0202:
+    #用法一：最常用的用法
+    @pytest.mark.parametrize('caseinfo',read_yaml('./testcases/test_api.yaml'))
+    def test_login(self,caseinfo):
+        print(f'登录测试用例:{caseinfo}')
+
+    #用法二：不常用，了解即可
+    @pytest.mark.parametrize('name,age',[['test1','11'],['test2','22']])
+    def test_register(self,name,age):
+        print('注册测试用例:%s' % name,age)
